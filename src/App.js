ae8b57dabcd7815b90bbd6262cbdc801118c797b
Nadia-si <nasimbi2011@gmail.com>
2023-06-19 21:31:37 +0200

    Added GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -2,5 +2,19 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project is coded by Nadia Simbi is open-sourced on{" "}
+        <a
+          href="https://github.com/Nadia-si/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Github
+        </a>
+      </footer>
+    </div>
+  );
 }
